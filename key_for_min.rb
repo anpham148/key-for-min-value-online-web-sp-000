32a172eac981417816962c155894979443515bc6
An Pham <anpham148@gmail.com>
2020-06-24 12:59:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,7 +13,10 @@ def key_for_min_value(name_hash)
      min_val = min_val < value ? min_val : value
    end
  end
- name_hash.collect {|key1, value1| min_val == value1 ? result = key1 }
-   
+ name_hash.collect do |key1, value1|
+   if min_val == value1
+     result = key1
+   end
+ end
  result
 end
